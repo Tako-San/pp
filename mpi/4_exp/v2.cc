d4689fc047e1be3bdc440a5d76da30c755fb1ccf
Tako-San <dxe13@mail.ru>
2023-02-18 09:48:46 +0300

    upd

--- mpi/4_exp/v2.cc
+++ mpi/4_exp/v2.cc
@@ -1,40 +1,13 @@
+#include <concepts>
 #include <cstddef>
 #include <iostream>
 #include <limits>
 
 #include <mpi.h>
 
-double worker(int N, int curRank, int ncpus) {
-  double fact = 1;
-  for (auto j = 2; j <= curRank; ++j)
-    fact *= j;
-
-  double partialSum = 0;
-  for (auto i = curRank + 1; i <= N; i += ncpus) {
-    auto term = 1 / fact;
-    for (auto j = i; j < i + ncpus; ++j)
-      fact *= j;
-
-    partialSum += term;
-  }
-
-  return partialSum;
-}
-
-void manager(int N, int curRank, int ncpus) {
-  double result = worker(N, curRank, ncpus);
-
-  for (int i = 1; i < ncpus; ++i) {
-    double tmp{};
-    MPI::COMM_WORLD.Recv(&tmp, 1, MPI::DOUBLE, i, 0);
-
-    result += tmp;
-  }
-
-  std::cout << "N = " << N << std::endl;
-  std::cout.precision(std::numeric_limits<double>::max_digits10);
-  std::cout << "e = " << result << std::endl;
-}
+template <std::integral T> T ceil(T dividend, T divider);
+double worker(int N, int curRank, int ncpus);
+void manager(int N, int curRank, int ncpus);
 
 int main(int ac, char **av) {
   if (ac < 2) {
@@ -57,4 +30,52 @@ int main(int ac, char **av) {
 
   MPI::Finalize();
   return 0;
+}
+
+template <std::integral T> T ceil(T dividend, T divider) {
+  return (dividend + divider - 1) / divider;
+}
+
+double worker(int N, int curRank, int ncpus) {
+  /* number of terms for each thread */
+  auto termN = ceil(N, ncpus);
+
+  double partialSum = 0;
+  double factTail = (curRank != 0) ? (curRank * termN) : 1;
+
+  auto start = curRank * termN + 1;
+  auto next = start + termN;
+  auto end = (next > N) ? (N + 1) : next;
+  for (auto n = start; n < end; ++n) {
+    partialSum += 1 / factTail;
+    factTail *= n;
+  }
+
+  return partialSum;
+}
+
+void manager(int N, int curRank, int ncpus) {
+  /* number of terms for each thread */
+  auto n = ceil(N, ncpus);
+
+  auto result = worker(N, curRank, ncpus);
+
+  decltype(result) fact = 1;
+  for (auto j = 2; j < n; ++j)
+    fact *= j;
+  /* here fact equals to (n - 1)! */
+
+  for (int i = 1; i < ncpus; ++i) {
+    decltype(result) tmp{};
+    MPI::COMM_WORLD.Recv(&tmp, 1, MPI::DOUBLE, i, 0);
+    result += tmp / fact;
+
+    for (auto j = i * n, end = (i + 1) * n; j < end; ++j)
+      fact *= j;
+    /* now fact equals to (i * n - 1)! */
+  }
+
+  std::cout << "N = " << N << std::endl;
+  std::cout.precision(std::numeric_limits<double>::max_digits10);
+  std::cout << "e = " << result << std::endl;
 }
