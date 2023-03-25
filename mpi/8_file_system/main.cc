3ea6401e5e76ecfca571adc04649224d2992085a
Tako-San <dxe13@mail.ru>
2023-03-25 18:28:03 +0300

    Added work with file system

---
+++ mpi/8_file_system/main.cc
@@ -0,0 +1,19 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int ac, char **av) {
+  MPI_File fh;
+
+  MPI::Init(ac, av);
+  auto rank = MPI::COMM_WORLD.Get_rank();
+  auto commSize = MPI::COMM_WORLD.Get_size();
+
+  MPI_File_open(MPI_COMM_WORLD, "test.out", MPI_MODE_CREATE | MPI_MODE_WRONLY,
+                MPI_INFO_NULL, &fh);
+
+  char c = '0' + commSize + rank;
+  MPI_File_write_at(fh, rank, &c, 1, MPI_CHAR, MPI_STATUS_IGNORE);
+  MPI_File_close(&fh);
+  MPI::Finalize();
+  return 0;
+}
