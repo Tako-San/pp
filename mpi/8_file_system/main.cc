444289f8f1b72ff62c33556b703cfea474837680
Tako-San <dxe13@mail.ru>
2023-03-25 18:44:03 +0300

    Generalized

--- mpi/8_file_system/main.cc
+++ mpi/8_file_system/main.cc
@@ -1,5 +1,6 @@
+#include <string>
+
 #include <mpi.h>
-#include <stdio.h>
 
 int main(int ac, char **av) {
   MPI::Init(ac, av);
@@ -10,8 +11,9 @@ int main(int ac, char **av) {
   MPI_File_open(MPI_COMM_WORLD, "test.out", MPI_MODE_CREATE | MPI_MODE_WRONLY,
                 MPI_INFO_NULL, &fh);
 
-  char c = '0' + commSize - rank;
-  MPI_File_write_at(fh, rank, &c, 1, MPI_CHAR, MPI_STATUS_IGNORE);
+  auto s = std::to_string(commSize - rank);
+  MPI_File_write_ordered(fh, s.data(), s.size(), MPI_CHAR, MPI_STATUS_IGNORE);
+
   MPI_File_close(&fh);
 
   MPI::Finalize();
