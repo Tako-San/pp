be2e1e787a2936863d79f442d2aa49ae82561835
Tako-San <dxe13@mail.ru>
2023-03-29 17:58:20 +0300

    V2

--- mpi/7_one_sided/main.cc
+++ mpi/7_one_sided/main.cc
@@ -2,49 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("Hey, Buddy, you choose the wrong door, the parallel club has two "
-           "args of command line\n");
+using T = double;
+
+int main(int ac, char **av) {
+  if (ac < 2) {
+    std::cout << "USAGE: " << av[0] << " N" << std::endl;
     return 0;
   }
 
-  int num = atoi(argv[1]);
+  auto N = atoi(av[1]);
+  MPI::Init(ac, av);
 
-  int commSize = 0, commRank = 0;
-  MPI_Win win;
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &commSize);
-  MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
+  auto commSize = MPI::COMM_WORLD.Get_size();
+  auto rank = MPI::COMM_WORLD.Get_rank();
 
   double sum = 0;
-  for (int i = num / commSize * commRank + 1,
-           end = (commRank == commSize - 1)
-                     ? num + 1
-                     : num / commSize * (commRank + 1) + 1;
-       i < end; ++i) {
-    sum += 1.0 / i;
+  for (auto n = rank + 1; n <= N; n += commSize) {
+    sum += 1.0 / n;
   }
 
-  MPI_Win_create(&sum, 1 * sizeof(double), sizeof(double), MPI_INFO_NULL,
-                 MPI_COMM_WORLD, &win);
+  auto win = MPI::Win::Create(&sum, sizeof(T), sizeof(T), MPI::INFO_NULL,
+                              MPI::COMM_WORLD);
 
-  MPI_Win_fence(0, win);
+  win.Fence(0);
+  if (rank != 0)
+    win.Accumulate(&sum, 1, MPI::DOUBLE, 0, 0, 1, MPI::DOUBLE, MPI::SUM);
+  win.Fence(0);
 
-  if (commRank != 0)
-    MPI_Accumulate(&sum, 1, MPI_DOUBLE, 0, 0, 1, MPI_DOUBLE, MPI_SUM, win);
+  if (0 == rank) {
+    std::cout << "N = " << N << std::endl;
+    std::cout << "sum = " << sum << std::endl;
+  }
 
-  MPI_Win_fence(0, win);
-
-  if (commRank == 0)
-    printf("result = %f\n", sum);
-
-  MPI_Win_fence(0, win);
-
-  MPI_Win_free(&win);
-  // MPI_Free_mem(result);
-  MPI_Finalize();
-
+  win.Free();
+  MPI::Finalize();
   return 0;
 }
