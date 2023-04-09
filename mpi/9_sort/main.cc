ae47a1f273b0e89896f82cdfe1c60b63d7d5114e
Tako-San <dxe13@mail.ru>
2023-04-09 13:45:55 +0300

    Done

--- mpi/9_sort/main.cc
+++ mpi/9_sort/main.cc
@@ -1,74 +1,59 @@
-/* IMPORTANT: Compile with -lm:
-   mpicc mpi_mergesort.c -lm -o mpi_mergesort */
-
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <vector>
 
 #include <mpi.h>
 
-// Arrays size <= SMALL switches to insertion sort
-#define SMALL 32
+using Vec = std::vector<int>;
+using VIter = Vec::iterator;
 
-void merge(int a[], int size, int temp[]);
-void insertionSort(int a[], int size);
-void mergeSortSerial(int a[], int size, int temp[]);
-void mergeSortParallel(std::vector<int> &a, std::vector<int> &temp, int size,
-                       int level, int rank, int maxRank, int tag,
-                       MPI::Comm &comm);
+constexpr int SMALL = 32;
+
 int topmostLevel(int rank);
-void runRootMPI(std::vector<int> &a, std::vector<int> &temp, int maxRank,
-                int tag, MPI::Comm &comm);
+void insertionSort(VIter a, int size);
+
 void runHelperMPI(int rank, int maxRank, int tag, MPI::Comm &comm);
-int main(int ac, char **av);
+void runRootMPI(Vec &a, Vec &tmp, int maxRank, int tag, MPI::Comm &comm);
+
+void merge(VIter a, VIter tmp, int size);
+void mergeSortSerial(VIter a, VIter tmp, int size);
+void mergeSortParallel(Vec &a, Vec &tmp, int size, int level, int rank,
+                       int maxRank, int tag, MPI::Comm &comm);
 
 int main(int ac, char **av) {
-  // All processes
   MPI::Init(ac, av);
 
-  // Check processes and their ranks
-  // number of processes == communicator size
   auto commsize = MPI::COMM_WORLD.Get_size();
-  auto myRank = MPI::COMM_WORLD.Get_rank();
+  auto rank = MPI::COMM_WORLD.Get_rank();
 
   auto maxRank = commsize - 1;
   int tag = 123;
 
   // Only root process sets test data
-  if (myRank != 0) {
-    runHelperMPI(myRank, maxRank, tag, MPI::COMM_WORLD);
+  if (rank != 0) {
+    runHelperMPI(rank, maxRank, tag, MPI::COMM_WORLD);
     MPI::Finalize();
     return 0;
   }
 
-  // Set test data
-  std::cout << "-MPI Recursive Mergesort-" << std::endl;
-
-  // Check arguments
   if (ac != 2) {
     std::cerr << "Usage: " << av[0] << " array-size" << std::endl;
     MPI::COMM_WORLD.Abort(1);
   }
 
-  // Get argument
-  auto size = atoi(av[1]); // Array size
+  auto arrSize = atoi(av[1]); // Array size
 
-  std::cout << "Array size = " << size << std::endl;
+  std::cout << "Array size = " << arrSize << std::endl;
   std::cout << "Processes = " << commsize << std::endl;
 
-  std::vector<int> a{};
-  a.resize(size);
-  auto tmp = a;
+  Vec a{};
+  a.resize(arrSize);
+  auto tmp{a};
 
   // Random array initialization
   srand(314159);
-  for (int i = 0; i < size; i++)
-    a[i] = rand() % size;
+  std::generate(a.begin(), a.end(), [arrSize] { return rand() % arrSize; });
 
   // Sort with root process
   auto start = MPI::Wtime();
@@ -87,8 +72,7 @@ int main(int ac, char **av) {
 }
 
 // Root process code
-void runRootMPI(std::vector<int> &a, std::vector<int> &temp, int maxRank,
-                int tag, MPI::Comm &comm) {
+void runRootMPI(Vec &a, Vec &tmp, int maxRank, int tag, MPI::Comm &comm) {
   auto rank = comm.Get_rank();
   if (rank != 0) {
     std::cerr << "Error: run_root_mpi called from process " << rank
@@ -96,7 +80,7 @@ void runRootMPI(std::vector<int> &a, std::vector<int> &temp, int maxRank,
     MPI::COMM_WORLD.Abort(1);
   }
 
-  mergeSortParallel(a, temp, a.size(), 0, rank, maxRank, tag, comm);
+  mergeSortParallel(a, tmp, a.size(), 0, rank, maxRank, tag, comm);
   /* level=0; rank=root_rank=0; */
   return;
 }
@@ -110,9 +94,9 @@ void runHelperMPI(int rank, int maxRank, int tag, MPI::Comm &comm) {
   auto size = status.Get_count(MPI::INT);
   auto parentRank = status.Get_source();
 
-  std::vector<int> a{};
+  Vec a{};
   a.resize(size);
-  std::vector<int> tmp = a;
+  Vec tmp = a;
 
   comm.Recv(a.data(), size, MPI::INT, parentRank, tag);
   mergeSortParallel(a, tmp, size, level, rank, maxRank, tag, comm);
@@ -132,76 +116,73 @@ int topmostLevel(int rank) {
 }
 
 // MPI merge sort
-void mergeSortParallel(std::vector<int> &a, std::vector<int> &temp, int size,
-                       int level, int rank, int maxRank, int tag,
-                       MPI::Comm &comm) {
-  auto helperRank = rank + pow(2, level);
+void mergeSortParallel(Vec &a, Vec &tmp, int size, int level, int rank,
+                       int maxRank, int tag, MPI::Comm &comm) {
+  auto helperRank = rank + static_cast<int>(pow(2, level));
   if (helperRank > maxRank) { // no more processes available
-    mergeSortSerial(a.data(), size, temp.data());
+    mergeSortSerial(a.begin(), tmp.begin(), size);
     return;
   }
-
-  // printf("Process %d has helper %d\n", rank, helperRank);
 
   // Send second half, asynchronous
   auto request = comm.Isend(a.data() + size / 2, size - size / 2, MPI::INT,
                             helperRank, tag);
   // Sort first half
-  mergeSortParallel(a, temp, size / 2, level + 1, rank, maxRank, tag, comm);
+  mergeSortParallel(a, tmp, size / 2, level + 1, rank, maxRank, tag, comm);
   // Free the async request (matching receive will complete the transfer).
   request.Free();
 
   // Receive second half sorted
   comm.Recv(a.data() + size / 2, size - size / 2, MPI::INT, helperRank, tag);
 
-  // Merge the two sorted sub-arrays through temp
-  merge(a.data(), size, temp.data());
+  // Merge the two sorted sub-arrays through tmp
+  merge(a.begin(), tmp.begin(), size);
 }
 
-void mergeSortSerial(int a[], int size, int temp[]) {
+void mergeSortSerial(VIter a, VIter tmp, int size) {
   // Switch to insertion sort for small arrays
   if (size <= SMALL) {
     insertionSort(a, size);
     return;
   }
-  mergeSortSerial(a, size / 2, temp);
-  mergeSortSerial(a + size / 2, size - size / 2, temp);
-  // Merge the two sorted subarrays into a temp array
-  merge(a, size, temp);
+  mergeSortSerial(a, tmp, size / 2);
+  mergeSortSerial(a + size / 2, tmp, size - size / 2);
+  // Merge the two sorted subarrays into a tmp array
+  merge(a, tmp, size);
 }
 
-void merge(int a[], int size, int temp[]) {
+void merge(VIter a, VIter tmp, int size) {
   int i1 = 0;
   int i2 = size / 2;
-  int tempi = 0;
+  int tmpi = 0;
   while (i1 < size / 2 && i2 < size) {
     if (a[i1] < a[i2]) {
-      temp[tempi] = a[i1];
+      tmp[tmpi] = a[i1];
       i1++;
     } else {
-      temp[tempi] = a[i2];
+      tmp[tmpi] = a[i2];
       i2++;
     }
-    tempi++;
+    tmpi++;
   }
   while (i1 < size / 2) {
-    temp[tempi] = a[i1];
+    tmp[tmpi] = a[i1];
     i1++;
-    tempi++;
+    tmpi++;
   }
   while (i2 < size) {
-    temp[tempi] = a[i2];
+    tmp[tmpi] = a[i2];
     i2++;
-    tempi++;
+    tmpi++;
   }
-  // Copy sorted temp array into main array, a
-  memcpy(a, temp, size * sizeof(int));
+  // Copy sorted tmp array into main array, a
+  std::copy_n(tmp, size, a);
 }
 
-void insertionSort(int a[], int size) {
-  int i;
-  for (i = 0; i < size; i++) {
-    int j, v = a[i];
+void insertionSort(VIter a, int size) {
+  for (int i = 0; i < size; i++) {
+    int j;
+    auto v = a[i];
     for (j = i - 1; j >= 0; j--) {
       if (a[j] <= v)
         break;
