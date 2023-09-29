c5ad9b971441200bc132f89126d3c4115c8eac03
Tako-San <dxe13@mail.ru>
2023-09-29 17:04:37 +0300

    5_copy finished

---
+++ sem7/OpenMP/5_copy/main.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+#include <sstream>
+
+#include <omp.h>
+
+int cin = -1;
+int cprivate = -1;
+
+#pragma omp threadprivate(cin)
+#pragma omp threadprivate(cprivate)
+
+void printThread(int cnt) {
+  std::ostringstream ss;
+  ss << "Thread " << omp_get_thread_num() << ", counter = " << cnt << std::endl;
+  std::cout << ss.str();
+}
+
+int main() {
+  std::cout << "Copyin:" << std::endl;
+#pragma omp parallel copyin(cin)
+  {
+    cin = omp_get_thread_num();
+    printThread(cin);
+  }
+
+  std::cout << std::endl << "Copyprivate:" << std::endl;
+#pragma omp parallel
+  {
+#pragma omp single copyprivate(cprivate)
+    cprivate = omp_get_thread_num();
+    printThread(cprivate);
+  }
+
+  std::cout << std::endl;
+  std::cout << "cin = " << cin << std::endl;
+  std::cout << "cprivate = " << cprivate << std::endl;
+}
