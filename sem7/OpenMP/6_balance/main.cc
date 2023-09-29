86fda6037863235182a6c4020b72728ae81272d7
Tako-San <dxe13@mail.ru>
2023-09-29 16:11:41 +0300

    Balance task done

---
+++ sem7/OpenMP/6_balance/main.cc
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <omp.h>
+#include <sstream>
+
+#define FOR_CYCLE(END)                                                         \
+  for (int i = 0; i < (END); ++i) {                                            \
+    std::ostringstream ss;                                                     \
+    int tid = omp_get_thread_num();                                            \
+    task_dist[tid][i] = true;                                                  \
+  }
+
+constexpr int end_cycle = 65;
+using MapTh = std::array<std::array<bool, end_cycle>, 4>;
+
+void clearMap(MapTh &map) {
+  for (auto &sub : map)
+    std::fill_n(sub.begin(), sub.size(), false);
+}
+
+void printMap(const MapTh &map) {
+  for (size_t tid = 0; tid < map.size(); ++tid) {
+    std::cout << "#" << tid << ": ";
+    for (size_t iter = 0; iter < map[tid].size(); ++iter)
+      if (map[tid][iter])
+        std::cout << iter << ' ';
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+  MapTh task_dist{};
+
+  omp_set_num_threads(4);
+  std::cout << "dynamic chunk 1" << std::endl;
+
+#pragma omp parallel for schedule(dynamic, 1)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "dynamic chunk 4" << std::endl;
+
+#pragma omp parallel for schedule(dynamic, 4)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "static chunk 1" << std::endl;
+
+#pragma omp parallel for schedule(static, 1)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "static chunk 4" << std::endl;
+
+#pragma omp parallel for schedule(static, 4)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "guided chunk 1" << std::endl;
+
+#pragma omp parallel for schedule(guided, 1)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "guided chunk 4" << std::endl;
+
+#pragma omp parallel for schedule(guided, 4)
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "default chunk 1" << std::endl;
+
+#pragma omp parallel for
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "default chunk 4" << std::endl;
+
+#pragma omp parallel for
+  FOR_CYCLE(end_cycle)
+
+  printMap(task_dist);
+  clearMap(task_dist);
+  std::cout << "END\n";
+}
